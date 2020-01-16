c554c58ce67cbc474f4aa203bcc7e802a72b88a9
markotron <markotron@gmail.com>
2020-01-16 10:08:26 +0100

    adding userId in url

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import './App.css';
 import {Chat} from "./chat/Chat";
 import {StarredMessages} from "./starred-messages/StarredMessages";
 import {Grid} from "@material-ui/core";
+import {useUrlSearchParams} from "use-url-search-params";
 
 function randUser() {
     return Math.round(Math.random() * 2);
 }
 
 function App() {
-    const user = randUser();
+    const [user] = useUrlSearchParams({userId: 0}, {userId: Number});
+    const userId: number = Number(user.userId);
     return (
         <div>
             <Grid container spacing={3}>
                 <Grid item xs={6}>
-                    <Chat me={user}/>
+                    <Chat me={userId}/>
                 </Grid>
                 <Grid item xs={6}>
-                    <StarredMessages me={user}/>
+                    <StarredMessages me={userId}/>
                 </Grid>
             </Grid>
         </div>
